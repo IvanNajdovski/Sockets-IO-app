7ca7367cbebd34a6a69a137186df663cc03634f8
Ivan Najdovski <ivan_najdovski@hotmail.com>
2018-06-17 12:30:56 +0200

    add css for chat page

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ io.on("connection", (socket) => {
                 console.log('New message :', message);
 
                 io.emit('newMessage', generateMessage(message.from, message.text));
-            callback('this is from the server');
+            callback();
                 });
 
             socket.on('createLocationMessage', (coords) => {
